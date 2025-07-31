15d3fa1ed88e419433214099e8928b781fe9e623
Luiz Arthur <luizarthurwinter@gmail.com>
2025-07-31 17:59:59 -0300

    creating the logic that saves the projects that we put into the inputs

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,16 +1,36 @@
 import Input from "./Input";
+import { useRef } from "react";
 
-export default function NewProject() {
+export default function NewProject({onAddProject}) {
+
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+
+    function handleSave(){
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
+
+        //validation...
+
+        onAddProject({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate,
+        })
+    }
+
     return (
     <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 mt-4">
             <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-            <li><button className="bg-stone-800 text-stone-50 px-6 py-2 rounded-md hover:bg-stone-950">Save</button></li>
+            <li><button onClick={handleSave} className="bg-stone-800 text-stone-50 px-6 py-2 rounded-md hover:bg-stone-950">Save</button></li>
         </menu>
         <div>
-            <Input label= "Title" type="text"></Input>
-            <Input label= "Description" isTextarea></Input>
-            <Input label= "Due Date"></Input>
+            <Input ref={title} label= "Title" type="text"></Input>
+            <Input ref={description} label= "Description" isTextarea></Input>
+            <Input ref={dueDate} label= "Due Date" type="date"></Input>
         </div>
 
     </div>
